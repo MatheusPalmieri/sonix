1d4fe43adcf929ee064b74d627896a7511a6111f
Matheus Palmieri <matheuspalmieridev@gmail.com>
2025-04-04 22:30:59 -0300

    refactor layout structure and enhance global styles for improved UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,20 @@ export default function RootLayout({
   children: ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="antialiased base">{children}</body>
+    <html lang='en'>
+      <body className='relative flex min-h-dvh flex-col space-y-6 p-6 antialiased'>
+        <div className='flex'>
+          <div>Logo</div>
+          <input type='text' placeholder='search' className='h-11' />
+        </div>
+
+        <div className='flex h-full flex-grow space-x-6'>
+          <div className='flex w-11 flex-col'>navbar</div>
+          <div className='flex flex-grow flex-col'>{children}</div>
+        </div>
+
+        <div className='absolute right-0 bottom-0 left-0 h-32'>player</div>
+      </body>
     </html>
   );
 }
