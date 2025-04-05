c4ff9eca2c4a5a507010a2f8d6cdcd6284a7d0fc
Matheus Palmieri <matheuspalmieridev@gmail.com>
2025-04-04 22:31:02 -0300

    update configuration files for Prettier and ESLint, enhance package dependencies, and standardize code formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript', 'plugin:prettier/recommended'),
+  {
+    plugins: ['prettier'],
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
 ];
 
 export default eslintConfig;
